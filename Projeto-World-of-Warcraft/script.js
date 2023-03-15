f371822752c565f50407b6f131eed9b0846e71ca
david william <ksgulosao@gmail.com>
2023-03-15 10:22:51 -0300

    new changes

--- Projeto-World-of-Warcraft/script.js
+++ Projeto-World-of-Warcraft/script.js
@@ -1,17 +1,21 @@
 const wheelBtns = document.querySelectorAll(".btn");
 const imgs = document.querySelectorAll(".img");
 const info = document.querySelectorAll(".info-container");
+let index = 0;
+let timer = 5000;
 
-wheelBtns.forEach((button, index) => {
+//changeImg();
+
+wheelBtns.forEach((button, i) => {
     button.addEventListener("click", () => {
         deactivateSelectedBtn();
         button.classList.add("selected");
 
         deactivateSelectedImg();
-        imgs[index].classList.add("active");
+        imgs[i].classList.add("active");
 
         deactivateSelectedInfo();
-        info[index].classList.add('info-active');
+        info[i].classList.add('info-active');
     })
 })
 
@@ -29,3 +33,23 @@ function deactivateSelectedInfo() {
     const selectedInfo = document.querySelector(".info-active");
     selectedInfo.classList.remove("info-active");
 }
+
+// function changeImg() {
+//     for (let i = 0; i < imgs.length; i++) {
+//         imgs[i].style.display = "none";
+//         wheelBtns[i].classList.remove("selected");
+//         info[i].classList.remove("info-active")
+//     }
+
+//     index++;
+
+//     if (index > imgs.length) {
+//         index = 1;
+//     }
+
+//     imgs[index - 1].style.display = "block";
+//     wheelBtns[index - 1].classList.add("selected");
+//     info[index - 1].classList.add("info-active");
+
+//     setTimeout(changeImg, timer);
+// }
