c094c0ab114b86ac3299c27ac80f36a92a702028
David William <ksdavid90@gmail.com>
2023-09-22 15:00:43 -0300

    some changes

---
+++ angular-blog/src/app/data/data-fake.ts
@@ -0,0 +1,32 @@
+// template
+// "id": 2,
+// "photo": "",
+// "title": "",
+// "description": ""
+
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "https://2.bp.blogspot.com/-mlHthmCWtpY/WG5gGpbIkKI/AAAAAAAAAr4/hv_le0NgrucBRZBCbuhF22sWHudUDyldwCLcB/s640/Capa.jpg",
+        "title": "Dragon Quest VIII no 3DS",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum?"
+    },
+    {
+        "id": "2",
+        "photo": "https://i.ytimg.com/vi/QNUQNRjjzEU/maxresdefault.jpg",
+        "title": "Difereças entre a versão de PLaystation 2 e a de 3DS",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum?"
+    },
+    {
+        "id": "3",
+        "photo": "https://i.ytimg.com/vi/Z2R7dFH4-gg/maxresdefault.jpg",
+        "title": "Rumores de que o Dragon Quest VIII poderá ser lançado até o ano que vem para o Nintendo 3DS",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum?"
+    },
+    {
+        "id": "4",
+        "photo": "https://www.gameinformer.com/s3/files/styles/body_default/s3/legacy-images/imagefeed/Moments%20%E2%80%93%20The%20Decision%20In%20Radiata%20Stories/radiatastoriesmoment610.jpg",
+        "title": "Você conhece Radiata Stories? Veja mais sobre essa pérola escondida do PS2",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quis similique, aspernatur id eaque fugiat quas suscipit fuga tenetur. Perferendis ducimus rem debitis quisquam adipisci quod sed, expedita nostrum harum?"
+    }
+]
