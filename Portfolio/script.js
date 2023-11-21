b2096a930dedc37ecfcdacb4b8377c7722067526
David William dos Santos <74513553+ksrogue@users.noreply.github.com>
2023-11-21 11:40:26 -0300

    a

---
+++ Portfolio/script.js
@@ -0,0 +1,21 @@
+// idade dinâmica --------------------------------;
+let data = new Date().getFullYear();
+let idade = data - 1997;
+document.querySelector(".idade").innerHTML = idade;
+// -----------------------------------------------;
+
+let projetos = document.querySelectorAll(".projetos");
+let buttons = document.querySelectorAll(".arrow-button");
+
+console.log(projetos);
+console.log(buttons);
+
+
+
+function nextProject() {
+    
+}
+
+function previousProject() {
+    
+}
